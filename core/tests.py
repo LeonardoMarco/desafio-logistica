19653a0bd3b753dbc1e7ef786c901a1b2e1420a6
LeonardoMarco <leo26alves@yahoo.com.br>
2025-01-23 22:44:38 -0300

    feat: create unit test and refactor view processor

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .utils import extract_data_from_line
 
-# Create your tests here.
+
+class TestExtractData(TestCase):
+    def test_data_length(self):
+        line = b'0000000088                             Terra Daniel DDS00000008360000000003     1899.0220210909\n'
+
+        result = extract_data_from_line(line)
+
+        self.assertEqual(len(result.user_id), 10, "user_id should have 10 characters")
+        self.assertEqual(len(result.name), 45, "name should have 45 characters (including spaces)")
+        self.assertEqual(len(result.order_id), 10, "order_id should have 10 characters")
+        self.assertEqual(len(result.product_id), 10, "product_id should have 10 characters")
+        self.assertEqual(len(result.product_value), 12, "product_value should have 12 characters (including decimal values)")
+        self.assertEqual(len(result.created_at.strip()), 8, "created_at should have 8 characters")
--- core/views.py
+++ core/views.py
@@ -5,6 +5,7 @@ from rest_framework import status
 from .utils import extract_data_from_line
 from .models import Client, Order, Product, ProductOrder
 from datetime import datetime
+from django.db import IntegrityError
 
 
 class FileProcessor(APIView):
@@ -13,34 +14,47 @@ class FileProcessor(APIView):
     def post(self, request, *args, **kwargs):
         uploaded_file = request.FILES.get('file', None)
 
-        if uploaded_file and uploaded_file.name.endswith('.txt'):
+        if uploaded_file and not uploaded_file.name.endswith('.txt'):
+            return Response({'error': 'No valid .txt file provided'}, status=status.HTTP_400_BAD_REQUEST)
+
+        id_errors = []
+        line_successfully = 0
+
+        for line in uploaded_file:
             try:
-                for line in uploaded_file:
-                    extracted_data = extract_data_from_line(line)
+                extracted_data = extract_data_from_line(line)
 
-                    client, _ = Client.objects.get_or_create(
-                        id=extracted_data.user_id,
-                        name=extracted_data.name,
-                    )
+                client, _ = Client.objects.get_or_create(
+                    id=extracted_data.user_id,
+                    name=extracted_data.name.strip(),
+                )
 
-                    order, _ = Order.objects.get_or_create(
-                        id=extracted_data.order_id,
-                        created_at=datetime.strptime(extracted_data.created_at, '%Y%m%d'),
-                        client=client
-                    )
+                order, _ = Order.objects.get_or_create(
+                    id=extracted_data.order_id,
+                    created_at=datetime.strptime(extracted_data.created_at.strip(), '%Y%m%d'),
+                    client=client
+                )
 
-                    product, _ = Product.objects.get_or_create(
-                        id=extracted_data.product_id
-                    )
+                product, _ = Product.objects.get_or_create(
+                    id=extracted_data.product_id
+                )
 
-                    ProductOrder.objects.create(
-                        order=order,
-                        product=product,
-                        value=extracted_data.product_value
-                    )
+                _, _ = ProductOrder.objects.get_or_create(
+                    order=order,
+                    product=product,
+                    value=extracted_data.product_value.strip()
+                )
 
-                return Response({'message': 'File processed successfully'}, status=status.HTTP_200_OK)
+                line_successfully += 1
+            except IntegrityError as e:
+                id_errors.append({'user_id': extracted_data.user_id, 'error': str(e)})
             except Exception as e:
                 return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
-        else:
-            return Response({'error': 'No valid .txt file provided'}, status=status.HTTP_400_BAD_REQUEST)
+        return Response(
+            {
+                'message': 'File processed successfully',
+                'line_successfully': line_successfully,
+                'id_errors': id_errors
+            },
+            status=status.HTTP_200_OK
+        )
